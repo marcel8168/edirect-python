ae770263df53dbbaad1fa13d5599e4a66580d0ba
marcel8168 <marcel_hiltner@protonmail.com>
2023-11-16 15:28:46 +0100

    protect api key

--- edirect_installation.sh
+++ edirect_installation.sh
@@ -9,4 +9,4 @@ echo "export PATH=/root/edirect:\${PATH}" >> ${HOME}/.bashrc
 # set the PATH for the current terminal session
 export PATH=${HOME}/edirect:${PATH}
 
-export NCBI_API_KEY="3a2f5b42e8ed2912591384c757e62c02d708"
+export NCBI_API_KEY=$(<api_key.txt)
