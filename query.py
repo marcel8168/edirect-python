bdb1c7b549c2a6946f866920b51172e800d647b3
marcel8168 <marcel_hiltner@protonmail.com>
2023-11-21 08:31:34 +0100

    [feature] further processing

--- query.py
+++ query.py
@@ -4,8 +4,19 @@
 import sys
 import os
 import shutil
+
+import pandas as pd
 import edirect
 from xml.etree.ElementTree import fromstring, ElementTree
+
+
+# query constants
+# journal abbreviation and ISSN available under https://ftp.ncbi.nih.gov/pubmed/J_Medline.txt
+# note that special characters in the query term lead to syntax errors
+JOURNAL = "Animals"
+JOURNAL_ISSN = "2076-2615"
+OUTPUT_FILE = 'animals_data.xml'
+
 
 xtract_path = shutil.which('xtract')
 
@@ -16,8 +27,7 @@ else:
 
 # for more information regarding the query see the guide: https://dataguide.nlm.nih.gov/edirect/xtract.html
 # or watch the webinar videos on YouTube: https://www.youtube.com/playlist?list=PL7dF9e2qSW0a6zx-yGMJvY6mcwQz_Vx4b
-
-query = 'esearch -db pubmed -query ""1533-4406"[JOUR] AND "N Engl J Med"[JOUR]" | \
+query = f'esearch -db pubmed -query ""{JOURNAL_ISSN}"[JOUR] AND "{JOURNAL}"[JOUR]" | \
     efetch -format xml | \
     xtract -set Set \
         -rec Rec -pattern PubmedArticle \
@@ -34,10 +44,23 @@ query = 'esearch -db pubmed -query ""1533-4406"[JOUR] AND "N Engl J Med"[JOUR]" 
 print("Query is being processed..")
 res = edirect.pipeline(query)
 
-if res:
-    file = 'result.xml'
-    res_xml = fromstring(res)
-    ElementTree(res_xml).write(file)
-    print(f"Query output written into {file}")
-else:
+if not res:
     print("No results available.")
+else:
+    res_xml = fromstring(res)
+    ElementTree(res_xml).write(OUTPUT_FILE)
+    print(f"Query output written into {OUTPUT_FILE}")
+
+    # Extract data from XML and create a DataFrame
+    data = []
+    for rec in res_xml.findall('.//Rec'):
+        common = rec.find('.//Common')
+        pmid = common.find('PMID').text
+        title = common.find('Title').text
+        abstract = common.find('Abstract').text
+        mesh_term_list = rec.find('.//MeshTermList')
+        mesh_terms = [term.text for term in mesh_term_list.findall('MeshTerm')]
+        data.append({'PMID': pmid, 'Title': title,
+                    'Abstract': abstract, 'MeshTermList': mesh_terms})
+
+    df = pd.DataFrame(data)
